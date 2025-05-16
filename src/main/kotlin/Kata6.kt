5c3c8e42e0861e5ab29cd329a1abafc8346cfbf4
Enrique López Mañas <eenriquelopez@gmail.com>
2025-05-16 15:54:43 +0200

    Kata 6

---
+++ src/main/kotlin/Kata6.kt
@@ -0,0 +1,12 @@
+fun checkInRange(date: MyDate, first: MyDate, last: MyDate): Boolean {
+    return TODO()
+}
+
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        if (year != other.year) return year - other.year
+        if (month != other.month) return month - other.month
+        return dayOfMonth - other.dayOfMonth
+    }
+}

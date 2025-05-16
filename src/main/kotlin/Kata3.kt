ab2891b0097c29cb7eb374e898e0ee663821c912
Enrique López Mañas <eenriquelopez@gmail.com>
2025-05-16 15:11:49 +0200

    Solution for Kata 3

--- src/main/kotlin/Kata3.kt
+++ src/main/kotlin/Kata3.kt
@@ -3,11 +3,10 @@ package org.example
 fun evaluate(expr: Expr): Int =
     when (expr) {
         is Num -> expr.value
-        is Sum ->  evaluate(expr.left) + evaluate(expr.right)
-        else -> throw IllegalArgumentException("Unknown expression")
+        is Sum -> evaluate(expr.left) + evaluate(expr.right)
     }
 
-interface Expr
+sealed interface Expr
 class Num(val value: Int) : Expr
 class Sum(val left: Expr, val right: Expr) : Expr
 

ced1811c234a3ff1b5fc9b4172fcc13da55e5633
Enrique López Mañas <eenriquelopez@gmail.com>
2025-05-16 15:54:29 +0200

    Solution for 5

--- src/main/kotlin/Kata5.kt
+++ src/main/kotlin/Kata5.kt
@@ -1,14 +1,18 @@
-fun Int.r(): RationalNumber = TODO()
+fun Int.r(): RationalNumber = RationalNumber(this, 1)
 
-fun Pair<Int, Int>.r(): RationalNumber = TODO()
+fun Pair<Int, Int>.r(): RationalNumber = RationalNumber(first, second)
 
 data class RationalNumber(val numerator: Int, val denominator: Int)
 
 fun main() {
-    val rationalNumber = 1.r()
+
+    val number = 1
+    val pair = Pair(1, 2)
+    println(number.r())
+    println(pair.r())
 }
 
 /**data class Pair<out A, out B>(
-    val first: A,
-    val second: B
+val first: A,
+val second: B
 )*/

496412c461633965c341145a79aa45e22389bedb
Enrique López Mañas <eenriquelopez@gmail.com>
2025-05-16 15:11:59 +0200

    Kata 4

---
+++ src/main/kotlin/Kata4.kt
@@ -0,0 +1,13 @@
+package org.example
+
+//as
+import kotlin.random.Random
+import java.util.Random
+
+fun useDifferentRandomClasses(): String {
+    return "Kotlin random: " +
+            KRandom.nextInt(2) +
+            " Java random:" +
+            JRandom().nextInt(2) +
+            "."
+}
